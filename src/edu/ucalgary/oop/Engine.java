d9bcb835fa3a5f4e1fdd538b4a25449d5ea1a03a
josh-yumul-ucal <josh.yumul@ucalgary.ca>
2026-02-10 16:16:25 -0700

    Update Engine.java

--- src/edu/ucalgary/oop/Engine.java
+++ src/edu/ucalgary/oop/Engine.java
@@ -1,5 +1,16 @@
 package edu.ucalgary.oop;
 
 public class Engine {
+    private String engineType;
 
+    public Engine(String engineType){
+        this.engineType = engineType;
+    }
+    public String getEngineType(){
+        return this.engineType;
+    }
+    public String getEngine(){
+        String engine = new Engine(engineType);
+        return engine;
+    }
 }
