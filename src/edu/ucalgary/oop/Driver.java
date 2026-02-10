5e0c0fd0574130611f9a48d8d7a511b4e71666b5
Ruthanne Gillis <ruthanne.gillis@ucalgary.ca>
2026-02-10 16:12:11 -0700

    driver class first

--- src/edu/ucalgary/oop/Driver.java
+++ src/edu/ucalgary/oop/Driver.java
@@ -1,5 +1,21 @@
 package edu.ucalgary.oop;
 
 public class Driver {
+    private string driverName;
 
+    public Driver(string name){
+        this.driverName = name;
+    }
+
+    public Driver(Driver other){
+        this.driverName = other;
+    }
+
+    public string getName(){
+        return driverName;
+    }
+
+    public setName(string name){
+        this.driverName = name;
+    }
 }
